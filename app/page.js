dce1d06fa58ef73322aad1b955b494c58d5d9096
Ɗყηᴀʍɨᴄ ◠‿◕ <79841865+DynamicAdi@users.noreply.github.com>
2025-10-15 15:24:39 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,16 +1,33 @@
-import React from "react";
+"use client";
+
+import React, {useState, useEffect} from "react";
 import ClientRender from "@/components/ClientRender";
 import { getPost } from "@/lib/calls";
 export const dynamic = 'force-dynamic';
 export const dynamicParams = true; 
 
 export default async function Main() {
-  const about = await getPost("about");
-  const services = await getPost("services");
-  const blogs = await getPost("blogcard");
-  const projects = await getPost("projects");
-  const footer = about[0].contactInformation;
-  const socialMedia = about[0].socialMediaLinks;
+  const [about, setAbout] = useState([]);  
+  const [services, setServices] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [footer, setFooter] = useState([]);
+    const [socialMedia, setSocialMedia] = useState([]);
+
+  useEffect(() => {
+  const abt = await getPost("about");
+    setAbout(abt)
+  const serv = await getPost("services");
+    setServices(serv)
+  const blog = await getPost("blogcard");
+    setBlogs(blog)
+  const project = await getPost("projects");
+    setProjects(project)
+  const foot = about[0].contactInformation;
+    setFooter(foot)
+  const social = about[0].socialMediaLinks;
+    setSocialMedia(social)
+  }, [])
 
   return (
     <ClientRender about={about} services={services} blogs={blogs} projects={projects} footer={footer} socialMedia={socialMedia} />
